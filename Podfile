5073488604cea9bd5cdff1f9c372167131e5a46e
jason <aizhaoyu@163.com>
2015-08-29 22:11:31 +0800

    add cocoa pods the AFNetworking

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+ platform :ios, '8.0'
+
+target 'swift weather' do
+pod 'AFNetworking'
+end
+
+target 'swift weatherTests' do
+
+end
+
---
+++ swift weather/objective_c_bridging_header.h
@@ -0,0 +1,13 @@
+//
+//  objective_c_bridging_header.h
+//  swift weather
+//
+//  Created by jason on 15/8/26.
+//  Copyright (c) 2015年 JasonAi. All rights reserved.
+//
+
+#ifndef swift_weather_objective_c_bridging_header_h
+#define swift_weather_objective_c_bridging_header_h
+
+
+#endif
